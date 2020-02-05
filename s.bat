8567ec4f9788cef8433749ab6c770341a1b597fc
Anuj Singh <43616495+anuj-singh14@users.noreply.github.com>
2020-02-05 13:52:02 +0530

    Add files via upload

---
+++ s.bat
@@ -0,0 +1,9 @@
+@echo off
+for /l %%x in (1,1,100000000) do (
+    echo %%x
+    gen %%x > in
+    tom < in > out1 
+    ass < in > out2
+    fc out1 out2 > diagnostics || exit /b
+)
+echo all tests passed
